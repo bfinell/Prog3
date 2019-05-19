2412d4459abdcd53c9f2e2e2fd044b842adc8769
bfinell <rsgod123>
2019-05-19 18:23:59 +0300

    added extra symbol and pearsonCorrelation

--- src/StockInfo.java
+++ src/StockInfo.java
@@ -1,48 +1,85 @@
 import com.google.gson.*;
 
-import java.io.File;
+import java.lang.reflect.Array;
+import  java.util.Calendar;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import org.apache.commons.math3.*;
+
+import com.sun.media.sound.InvalidFormatException;
+import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
 import org.ini4j.*;
 
+import javax.lang.model.util.ElementScanner6;
+
 public class StockInfo {
-   private String symbol;
+   private String symbol,symbol2;
    private String size;
-   private   String dSeries;
-   private   String tSeries;
-   private   String interval;
-   private  String apiKey;
-   private   ArrayList<JsonObject> data = new ArrayList<>();
+   private String dSeries;
+   private String tSeries;
+   private String interval;
+   private String apiKey;
+   private ArrayList<JsonObject> data = new ArrayList<>();
    private ArrayList<String> keyset = new ArrayList<>();
-   private   ArrayList<String> open = new ArrayList<>();
+    private ArrayList<String> keyset2 = new ArrayList<>();
+    private ArrayList<String> open = new ArrayList<>();
+   private ArrayList<String> open2 = new ArrayList<>();
+   private String start,end;
+   private String finalURL,finalURL2;
 
-
-
-    public StockInfo(String dSeries, String tSeries, String symbol, String interval, String size,String apiKey){
+//,
+    public StockInfo(String dSeries, String tSeries, String symbol,String symbol2, String interval, String size,String apiKey,String start, String end)throws InvalidFormatException{
             this.dSeries=dSeries;
             this.tSeries=tSeries;
             this.symbol=symbol;
+            this.symbol2=symbol2;
             this.interval=interval;
             this.size=size;
             this.apiKey = apiKey;
+            this.start = (start);
+            this.end = (end);
+            System.out.println(start + "  end  " + end);
 
         }
+      /*  private Calendar calendarFromString(String string)throws InvalidFormatException{
+            String[] s = string.split("\\.");
+            Calendar calendar;
+            if (s.length == 3){
+                calendar = new Calendar.Builder()
+                        .setDate(Integer.parseInt(s[2]),Integer.parseInt(s[1])-1, Integer.parseInt(s[0]))
+                        .build();
+            }else throw new InvalidFormatException("Date format not valid!");
+            return calendar;
+        }*/
 // https://www.alphavantage.co/query?function=Time_SERIES_INTRADAY&symbol=MSFT&outputsize=full&apikey=ZR69NHOOT7AMCZH8
 //https://www.alphavantage.co/query?function=Time_SERIES_INTRADAY&symbol=MSFT&interval=60min&outputsize=full&apikey=ZR69NHOOT7AMCZH8
-        private String URLBuilder(){
-        String finalURL ="";
-
+        private void URLBuilder(){
+            if(symbol2!=""){
+                if (this.tSeries.equals("TIME_SERIES_INTRADAY")){
+                    finalURL2= "https://www.alphavantage.co/query?function=" +
+                            tSeries+"&symbol=" +
+                            symbol2+"&interval=" +
+                            interval+"&outputsize=" +
+                            size+"&apikey="+apiKey;
+                }
+                else {
+                    finalURL2 =
+                            "https://www.alphavantage.co/query?function=" +
+                                    tSeries+"&symbol=" +
+                                    symbol2+"&outputsize=" +
+                                    size+"&apikey="+apiKey;//=ZR69NHOOT7AMCZH8";
+                }
+            }
         if (this.tSeries.equals("TIME_SERIES_INTRADAY")){
              finalURL= "https://www.alphavantage.co/query?function=" +
                     tSeries+"&symbol=" +
                     symbol+"&interval=" +
                     interval+"&outputsize=" +
                     size+"&apikey="+apiKey;
-            return finalURL;
         }
           else {
               finalURL =
@@ -50,17 +87,14 @@ public class StockInfo {
                               tSeries+"&symbol=" +
                               symbol+"&outputsize=" +
                               size+"&apikey="+apiKey;//=ZR69NHOOT7AMCZH8";
-              return finalURL;
 
         }
 
     }
-
-
     public void getData(){
-        System.out.println(URLBuilder());
+            URLBuilder();
         try {
-            URL url = new URL(URLBuilder());
+            URL url = new URL(finalURL);
             URLConnection request = url.openConnection();
             request.connect();
 
@@ -79,8 +113,6 @@ public class StockInfo {
             for (int i = 0; i<obj.get(tempObj).getAsJsonObject().size(); i++){
                 keyset.add(parts[i]);
             }
-
-
             open.add(obj.get(tempObj).getAsJsonObject().get(keyset.get(0)).getAsJsonObject().get(dSeries).toString());
             for (int i = 1; i<keyset.size();i++){
                     open.add(obj.get(tempObj).getAsJsonObject().get(keyset.get(i).substring(1)).getAsJsonObject().get(dSeries).toString());
@@ -91,15 +123,66 @@ public class StockInfo {
         }catch (IOException | NullPointerException e){
 
         }
+
+        if ((finalURL2!=null)){
+            System.out.println(finalURL2);
+            try{
+                URL url = new URL(finalURL2);
+                URLConnection request = url.openConnection();
+                request.connect();
+
+                JsonParser jp = new JsonParser();
+                JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+                JsonObject obj = root.getAsJsonObject();
+
+                String[] tempObjPart = obj.keySet().toString().split(",");
+                String tempObj = tempObjPart[1].replace("]","").substring(1);
+                System.out.println(tempObj);
+                data.add(obj.get(tempObj).getAsJsonObject());
+                String prePart = obj.get(tempObj).getAsJsonObject().keySet().toString().replace("[","") ;
+
+                String [] parts = prePart.split(",");
+
+                for (int i = 0; i<obj.get(tempObj).getAsJsonObject().size(); i++){
+                    keyset2.add(parts[i]);
+                }
+                open2.add(obj.get(tempObj).getAsJsonObject().get(keyset2.get(0)).getAsJsonObject().get(dSeries).toString());
+                for (int i = 1; i<keyset2.size();i++){
+                    open2.add(obj.get(tempObj).getAsJsonObject().get(keyset2.get(i).substring(1)).getAsJsonObject().get(dSeries).toString());
+
+                }
+                open2.add(obj.get(tempObj).getAsJsonObject().get(keyset2.get(keyset2.size()-1).substring(1,20)).getAsJsonObject().get(dSeries).toString());
+
+            }catch (IOException | NullPointerException e){
+
+            }
         }
 
-        protected ArrayList<String> getDate(){
-            return this.keyset;
-        }
-        protected ArrayList<String> getOpen(){
-
-        return open;
     }
+        protected int getStart(){
+        String temp = start.replace(".","");
+        return Integer.parseInt(temp);
+        }
+        protected int getEnd(){
+        String temp = end.replace(".","");
+            System.out.println(temp);
+        return Integer.parseInt(temp);
+        }
+
+        protected double pearsonCorrelation(){
+
+            double[] a=new double[open.size()];
+            double[] b=new double[open2.size()];
+            for (int i = 0; i<open.size(); i++){
+                a[i]=Double.parseDouble(open.get(i).replace("\"",""));
+                b[i]=Double.parseDouble(open2.get(i).replace("\"", ""));
+            }
+            double corr = new PearsonsCorrelation().correlation(a,b);
+            return corr;
+        }
+        protected ArrayList<String> getDate(){return this.keyset;}
+        protected ArrayList<String> getOpen(){return open;}
+        protected ArrayList<String> getOpen2(){return open2;}
 
 
 }
--- src/ViewSceneController.java
+++ src/ViewSceneController.java
@@ -7,11 +7,12 @@ import javafx.scene.control.ComboBox;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import org.ini4j.*;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import com.sun.media.sound.InvalidFormatException;
 
 
 public class ViewSceneController {
@@ -26,6 +27,8 @@ public class ViewSceneController {
     @FXML
     private ComboBox<String> symbol;
     @FXML
+    private ComboBox<String> symbol2;
+    @FXML
     private ComboBox<String> timeInterval;
     @FXML
     private ComboBox<String> size;
@@ -33,11 +36,18 @@ public class ViewSceneController {
     private ComboBox<String> API_KEY;
     @FXML
     private LineChart<Number,Number> graph;
+    @FXML
+    private TextField startDate;
+    @FXML
+    private TextField stopDate;
+    @FXML
+    private TextField pearson;
 
     ObservableList<String> apiKey = FXCollections.observableArrayList();
     ObservableList<String> dList = FXCollections.observableArrayList();
     ObservableList<String> tsList = FXCollections.observableArrayList();
     ObservableList<String> symbolList = FXCollections.observableArrayList();
+    ObservableList<String> symbol2List = FXCollections.observableArrayList();
     ObservableList<String> tiList = FXCollections.observableArrayList();
     ObservableList<String> sizeList = FXCollections.observableArrayList();
 
@@ -57,6 +67,7 @@ public class ViewSceneController {
     String sString = ini.get("controllInfo","SYMBOL");
     String[] sInfo = sString.split(",");
     symbolList.addAll(sInfo);
+    symbol2List.addAll(sInfo);
 
     String tiString = ini.get("controllInfo","TIME_INTERVAL");
     String[] tiInfo = tiString.split(",");
@@ -73,17 +84,23 @@ public class ViewSceneController {
         dataSeries.setItems(dList);
         timeSeries.setItems(tsList);
         symbol.setItems(symbolList);
+        symbol2.setItems(symbol2List);
         timeInterval.setItems(tiList);
         size.setItems(sizeList);
     }
 
 
     @FXML
-    protected void handledoQueryAction(ActionEvent event) {
-        StockInfo info = new StockInfo(dataSeries.getValue(),timeSeries.getValue(), symbol.getValue(),timeInterval.getValue() ,size.getValue(),API_KEY.getValue());
+    protected void handledoQueryAction(ActionEvent event) throws InvalidFormatException{
+        tArea.clear();
+        StockInfo info = new StockInfo(dataSeries.getValue(),timeSeries.getValue(), symbol.getValue(),symbol2.getValue(),timeInterval.getValue() ,size.getValue(),API_KEY.getValue(),startDate.getText(),stopDate.getText());
         info.getData();
-        setData(info.getDate(),info.getOpen());
-        chartSetter(info.getDate(),info.getOpen());
+        setData(info.getDate(),info.getOpen(),info.getOpen2(),info.getStart(),info.getEnd());
+        chartSetter(info.getDate(),info.getOpen(),info.getOpen2(),info.getStart(),info.getEnd());
+        if (!info.getOpen2().isEmpty()){
+            setPearson(info.pearsonCorrelation());
+        }
+
     }
     @FXML
     private void handleTimeSeriesAction(ActionEvent event){
@@ -102,34 +119,70 @@ public class ViewSceneController {
     }
 
 
-
-    private void setData(ArrayList date, ArrayList price) {
+    private void setData(ArrayList date, ArrayList price,ArrayList price2,int start, int stop) {
      date.remove(date.size()-1);
         System.out.println(price.size());
-            for (int i =price.size()-1 ; i >=0; i--) {
-                tArea.appendText("Date:" + date.get(i) +" "+ dataSeries.getValue().toString().substring(3)+ " "+ price.get(i).toString().replace("\"","") + "\n");
+        System.out.println(date.get(2));
 
+        if (symbol2.getValue()!=""){
+            for (int i =price.size()-1 ; i >=0; i--) {
+
+                int temp = Integer.parseInt(date.get(i).toString().replace("-","").replaceAll(" ","").substring(0,8));
+                if (temp >=start && temp <= stop) {
+                    tArea.appendText("Date:" + date.get(i) + " " + dataSeries.getValue().substring(3) + "  "+symbol.getValue()+" " + price.get(i).toString().replace("\"", "") + "   "+symbol2.getValue()+"" +
+                            ": "+price2.get(i).toString().replace("\"", "")+"\n");
+                }
             }
 
+        }else {
+            for (int i = price.size() - 1; i >= 0; i--) {
+
+                int temp = Integer.parseInt(date.get(i).toString().replace("-", "").replaceAll(" ", "").substring(0, 8));
+                if (temp >= start && temp <= stop) {
+                    tArea.appendText("Date:" + date.get(i) + " " + dataSeries.getValue().substring(3) + "  " + symbol.getValue() + ": " + price.get(i).toString().replace("\"", "") +"\n");
+                }
+            }
+        }
     }
+
     //mom5
-    private void chartSetter(ArrayList date, ArrayList price){
+    private void chartSetter(ArrayList date, ArrayList price,ArrayList price2, int start, int stop) {
 
         this.graph.getData().clear();
 
-        System.out.println(date.get(1).toString().replace("-",".").replace(":","").replace(" ","").substring(5,10));
+        System.out.println(date.get(1).toString().replace("-", ".").replace(":", "").replace(" ", "").substring(5, 10));
 
-        XYChart.Series<Number,Number> series = new XYChart.Series<>();
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        series.setName(symbol.getValue());
+
         this.graph.getData().add(series);
-        for (int i = 1;i< date.size()-2; i++){
-            String tempD = date.get(i).toString().replace("-",".").replace(":","").replace(" ","").substring(5,10   );
-            String tempP = price.get(i).toString().replace("\"","");
-            Float p = Float.parseFloat(tempP);
-            series.getData().add(new XYChart.Data(tempD,p));
+        for (int i = 1; i < date.size() - 2; i++) {
+            int temp = Integer.parseInt(date.get(i).toString().replace("-", "").replaceAll(" ", "").substring(0, 8));
+            if (temp >= start && temp <= stop) {
+                String tempD = date.get(i).toString().replace("-", ".").replace(":", "").replace(" ", "").substring(5, 10);
+                String tempP = price.get(i).toString().replace("\"", "");
+                Float p = Float.parseFloat(tempP);
+                series.getData().add(new XYChart.Data(tempD, p));
+            }
         }
 
-
+        if (!price2.isEmpty()) {
+            XYChart.Series<Number, Number> series2 = new XYChart.Series<>();
+            series2.setName(symbol2.getValue());
+            this.graph.getData().add(series2);
+            for (int i = 1; i < date.size() - 2; i++) {
+                int temp = Integer.parseInt(date.get(i).toString().replace("-", "").replaceAll(" ", "").substring(0, 8));
+                if (temp >= start && temp <= stop) {
+                    String tempD = date.get(i).toString().replace("-", ".").replace(":", "").replace(" ", "").substring(5, 10);
+                    String tempP = price2.get(i).toString().replace("\"", "");
+                    Float p = Float.parseFloat(tempP);
+                    series2.getData().add(new XYChart.Data(tempD, p));
+                }
+            }
+        }
 
     }
-
+    private void setPearson(double value){
+        pearson.setText(Double.toString(value));
+    }
 }
